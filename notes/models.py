c1721971c583677056527dd0a8e74765804c69fa
Kirabo Ibrahim <kiraboibra268@gmail.com>
2023-06-03 13:18:04 +0300

    refactor: Add ordering based on the updated and created time

--- notes/models.py
+++ notes/models.py
@@ -9,6 +9,12 @@ from encrypted_model_fields.fields import EncryptedTextField
 User = get_user_model()
 
 
+class NotesQueryset(models.query.QuerySet):
+
+    def for_user(self, user):
+        return self.filter(owner=user)
+
+
 class Note(models.Model):
     title = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(editable=False, max_length=50)
@@ -16,12 +22,22 @@ class Note(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = NotesQueryset.as_manager()
 
-    def body_as_html(self):
+    def as_html(self):
         md_extensions = ["codehilite", "nl2br", "fenced_code", "sane_lists", "wikilinks", "toc"]
         return mark_safe(
-            markdown.markdown(self.body, extensions=md_extensions)
+            markdown.markdown(self.body, extensions=md_extensions, output_format="html")
         )
+
+    def as_pdf(self):
+        """TODO: Allow users to download note as pdf"""
+        pass
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        ordering = ("-updated_at", "-created_at")
+
+
